3165a5a86c8230c461236eaf095438c3829955bb
Young998904 <like0998904@gmail.com>
2022-09-22 09:46:45 +0900

    :tada: 프로젝트 추가 셋팅 (2)

--- src/main/java/com/ll/exam/app_2022_09_22/App20220922Application.java
+++ src/main/java/com/ll/exam/app_2022_09_22/App20220922Application.java
@@ -1,9 +1,12 @@
 package com.ll.exam.app_2022_09_22;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// 배치 사용을 위한 필수 설정 -> 관련 내용들이 db 에 자동으로 생성됨
+@EnableBatchProcessing
 public class App20220922Application {
 
 	public static void main(String[] args) {
