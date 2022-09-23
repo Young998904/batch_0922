5ae37114000f13a233cf06981fbb7e63c7bde659
Young998904 <like0998904@gmail.com>
2022-09-23 14:35:02 +0900

    :sparkles: Tasklet 에 파라미터 넘기기

--- src/main/java/com/ll/exam/app_2022_09_22/job/withParam/WithParamJobConfig.java
+++ src/main/java/com/ll/exam/app_2022_09_22/job/withParam/WithParamJobConfig.java
@@ -9,6 +9,7 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,25 +20,28 @@ public class WithParamJobConfig {
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean // 실행 시 수집
-    public Job withParamJob() {
+    public Job withParamJob(Step withParamStep1) {
         return jobBuilderFactory.get("withParamJob")
-            .start(withParamStep1())
+            .start(withParamStep1)
             .build();
     }
     @Bean
     @JobScope // 해당 JOB 이 실행될 때 생성
-    public Step withParamStep1() {
+    public Step withParamStep1(Tasklet withParamStep1Tasklet) {
         return stepBuilderFactory
             .get("withParamStep1")
-            .tasklet(withParamStep1Tasklet())// 방법 (1) tasklet (2) item
+            .tasklet(withParamStep1Tasklet)// 방법 (1) tasklet (2) item
             .build();
     }
 
     @Bean
     @StepScope // 해당 Step 이 실행될 때 생성
-    public Tasklet withParamStep1Tasklet () {
+    public Tasklet withParamStep1Tasklet (
+        @Value ("#{jobParameters['age']}") int age,
+        @Value ("#{jobParameters['name']}") String name
+    ) {
         return (contribution, chunkContext) -> {
-            System.out.println("위드 파람 테스크릿 1!");
+            System.out.println("WithParam 테스트릿 1, %s, %d".formatted(name,age));
 
             return RepeatStatus.FINISHED;
         };
