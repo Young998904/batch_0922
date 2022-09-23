df21489d8798fb917d37f22be0858ccb40981a79
Young998904 <like0998904@gmail.com>
2022-09-23 13:18:36 +0900

    :sparkles: step 추가

--- src/main/java/com/ll/exam/app_2022_09_22/job/helloWorld/HelloWorldJobConfig.java
+++ src/main/java/com/ll/exam/app_2022_09_22/job/helloWorld/HelloWorldJobConfig.java
@@ -27,8 +27,9 @@ public class HelloWorldJobConfig {
         // get 은 얻다가 아닌 생성의 의미로 해석!
         // get 은 builder 를 가지고 오는 것이다.
         return jobBuilderFactory.get("helloWorldJob")
-//            .incrementer(new RunIdIncrementer()) // 강제로 매번 다른 ID를 실행시에 파라미터로 부여
+            .incrementer(new RunIdIncrementer()) // 강제로 매번 다른 ID를 실행시에 파라미터로 부여
             .start(helloWorldStep1 ()) // Step 들을 넣는 부분
+            .next(helloWorldStep2())// helloWorldStep1 이 종료되면 다음에 수행됨
             .build();
     }
 
@@ -38,17 +39,35 @@ public class HelloWorldJobConfig {
     public Step helloWorldStep1 () {
         return stepBuilderFactory
             .get("helloWorldStep1")
-            .tasklet(helloWorldTasklet())// 방법 (1) tasklet (2) item
+            .tasklet(helloWorldStep1Tasklet())// 방법 (1) tasklet (2) item
+            .build();
+    }
+    @Bean
+    @JobScope // 해당 JOB 이 실행될 때 생성
+    public Step helloWorldStep2 () {
+        return stepBuilderFactory
+            .get("helloWorldStep2")
+            .tasklet(helloWorldStep2Tasklet())// 방법 (1) tasklet (2) item
             .build();
     }
 
+    // 테스크릿 생성
     @Bean
     @StepScope // 해당 Step 이 실행될 때 생성
-    public Tasklet helloWorldTasklet () {
+    public Tasklet helloWorldStep1Tasklet () {
         return (contribution, chunkContext) -> {
-                System.out.println("헬로 월드!");
+                System.out.println("헬로 월드 테스크릿 1!");
 
                 return RepeatStatus.FINISHED;
+        };
+    }
+    @Bean
+    @StepScope // 해당 Step 이 실행될 때 생성
+    public Tasklet helloWorldStep2Tasklet () {
+        return (contribution, chunkContext) -> {
+            System.out.println("헬로 월드 테스크릿 2!");
+
+            return RepeatStatus.FINISHED;
         };
     }
 
