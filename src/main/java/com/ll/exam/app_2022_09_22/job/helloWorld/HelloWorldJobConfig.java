00855273c021938c714744cbfc4c30bb61c9e4ad
Young998904 <like0998904@gmail.com>
2022-09-23 13:39:28 +0900

    :test_tube: helloWorldJob 의 step2 일부러 실패하도록

--- src/main/java/com/ll/exam/app_2022_09_22/job/helloWorld/HelloWorldJobConfig.java
+++ src/main/java/com/ll/exam/app_2022_09_22/job/helloWorld/HelloWorldJobConfig.java
@@ -7,7 +7,6 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -27,7 +26,7 @@ public class HelloWorldJobConfig {
         // get 은 얻다가 아닌 생성의 의미로 해석!
         // get 은 builder 를 가지고 오는 것이다.
         return jobBuilderFactory.get("helloWorldJob")
-            .incrementer(new RunIdIncrementer()) // 강제로 매번 다른 ID를 실행시에 파라미터로 부여
+//            .incrementer(new RunIdIncrementer()) // 강제로 매번 다른 ID를 실행시에 파라미터로 부여
             .start(helloWorldStep1 ()) // Step 들을 넣는 부분
             .next(helloWorldStep2())// helloWorldStep1 이 종료되면 다음에 수행됨
             .build();
@@ -56,7 +55,11 @@ public class HelloWorldJobConfig {
     @StepScope // 해당 Step 이 실행될 때 생성
     public Tasklet helloWorldStep1Tasklet () {
         return (contribution, chunkContext) -> {
-                System.out.println("헬로 월드 테스크릿 1!");
+                System.out.println("헬로 월드 테스크릿 1 수정본!");
+
+                if (true) {
+                    throw new Exception("실패 : 테스크릿 1");
+                }
 
                 return RepeatStatus.FINISHED;
         };
@@ -66,6 +69,10 @@ public class HelloWorldJobConfig {
     public Tasklet helloWorldStep2Tasklet () {
         return (contribution, chunkContext) -> {
             System.out.println("헬로 월드 테스크릿 2!");
+
+            if (false) {
+                throw new Exception("실패 : 헬로 월드 테스크릿 2");
+            }
 
             return RepeatStatus.FINISHED;
         };
