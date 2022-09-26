73bb957ff1c58dffd57412c32cd470fddde13177
Young998904 <like0998904@gmail.com>
2022-09-26 23:48:49 +0900

    :bug: 같은 옵션의 제품을 장바구니에 담을 경우 기존 수량에서 값을 더하도록

--- src/main/java/com/ll/exam/app_2022_09_22/app/cart/repository/CartItemRepository.java
+++ src/main/java/com/ll/exam/app_2022_09_22/app/cart/repository/CartItemRepository.java
@@ -1,8 +1,10 @@
 package com.ll.exam.app_2022_09_22.app.cart.repository;
 
 import com.ll.exam.app_2022_09_22.app.cart.entity.CartItem;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
+    Optional<CartItem> findByMemberIdAndProductOptionId(Long memberId, Long productOptionId);
 }
