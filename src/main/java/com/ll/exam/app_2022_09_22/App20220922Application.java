caa08c9ddca35fb839ed33abbd59671763e9d82f
Young998904 <like0998904@gmail.com>
2022-09-26 11:01:42 +0900

    :card_file_box: member DevInitData 생성

--- src/main/java/com/ll/exam/app_2022_09_22/App20220922Application.java
+++ src/main/java/com/ll/exam/app_2022_09_22/App20220922Application.java
@@ -3,10 +3,12 @@ package com.ll.exam.app_2022_09_22;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 // 배치 사용을 위한 필수 설정 -> 관련 내용들이 db 에 자동으로 생성됨
 @EnableBatchProcessing
+@EnableJpaAuditing
 public class App20220922Application {
 
 	public static void main(String[] args) {
